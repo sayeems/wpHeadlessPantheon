326cd3c0cdd8c2cbb312e21fc47b94de544a1c3e
sayeem <sayeems@gmail.com>
2021-11-30 23:45:56 +0600

    floating button

--- src/Products.js
+++ src/Products.js
@@ -1,28 +1,33 @@
-import {Grid, 
-    Typography, 
-    Container, 
-    Paper, 
-    Divider, 
+import {
+    Grid,
+    Typography,
+    Container,
+    Paper,
+    Divider,
     IconButton,
-    Button, 
-    Stack, 
-    Badge, 
-    List, 
-    ListItem, 
-    ListItemButton, 
+    Button,
+    Stack,
+    Badge,
+    List,
+    ListItem,
+    ListItemButton,
     ListItemIcon,
     ListItemText,
     Card,
     CardActions,
     CardContent,
     CardMedia,
-    Skeleton
+    Skeleton,
+    Rating,
+    SpeedDial,
+    SpeedDialIcon,
+    SpeedDialAction
 } from '@mui/material';
-import {FormatListBulleted, Category} from '@mui/icons-material';
+import { AddShoppingCart, Visibility, Favorite } from '@mui/icons-material';
 import axios from 'axios';
-import {useState, useEffect, useContext} from 'react';
-import {Link} from 'react-router-dom';
-import {CartContext} from './App';
+import { useState, useEffect, useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { CartContext } from './App';
 
 const Products = (props) => {
 
@@ -44,63 +49,78 @@ const Products = (props) => {
             setLoading(false);
         }
         return fetchProduct();
-    },[]);
-//sayeem
+    }, []);
+    //sayeem
 
-    return ( 
-        <Grid item xs={10} style={{paddingLeft:'15px'}}>
+    return (
+        <Grid item xs={10} style={{ paddingLeft: '15px' }}>
             <h3>Products</h3>
             <Divider />
             <Grid container spacing={2}>
-                {!loading && 
-                products.map(item=>
-                    <Grid item xs={4} style={{marginTop:'10px'}} key={item.id}>
-                        <Card>
-                            <CardMedia
-                            component="img"
-                            height="300"
-                            image={item.images[0].src}
-                            >
-                            </CardMedia>
-                            <CardContent>
-                            <Typography gutterBottom variant="h5" component="div">
-                                {item.name}
-                            </Typography>
-                            <Typography gutterbottom variant="p" component="div">
-                                Price: {item.price}
-                            </Typography>
-                            <Typography gutterbottom variant="p" component="div">
-                                Rating: {item.average_rating}
-                            </Typography>
-                            </CardContent>
-                            <CardActions>
-                            <Button variant="contained" color="primary" to={`/${item.id}`} component={Link}>
-                                Details
-                            </Button>
-                            <Button onClick={add} style={{marginLeft:'15px'}} variant="contained" color="error">
-                                Add to Cart
-                            </Button>
-                        </CardActions>
-                        </Card>
-                    </Grid>
-                )
+                {!loading &&
+                    products.map(item =>
+                        <Grid item xs={4} style={{ marginTop: '10px' }} key={item.id}>
+                            <Card sx={{ position: 'relative' }}>
+                                <CardMedia
+                                    component="img"
+                                    height="300"
+                                    image={item.images[0].src}
+                                >
+                                </CardMedia>
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5" component="div">
+                                        {item.name}
+                                    </Typography>
+                                    <Typography gutterbottom variant="p" component="div">
+                                        Price: {item.price}
+                                    </Typography>
+                                    <Typography gutterbottom variant="p" component="div">
+                                        <Rating name="read-only" value={item.average_rating} readOnly />
+                                    </Typography>
+                                </CardContent>
+                                <SpeedDial
+                                    ariaLabel="ecommerce actions"
+                                    sx={{ position: 'absolute', bottom: 16, right: 16 }}
+                                    icon={<SpeedDialIcon />}
+                                    size="small"
+                                >
+                                    <SpeedDialAction
+                                        icon={<AddShoppingCart />}
+                                        tooltipTitle="Add to Cart"
+                                        onClick={add}
+                                    />
+                                    <SpeedDialAction
+                                        icon={<Visibility />}
+                                        tooltipTitle="View Details"
+                                        to={`/${item.id}`}
+                                        component={Link}
+                                    />
+                                    <SpeedDialAction
+                                        icon={<Favorite />}
+                                        tooltipTitle="Favourite"
+                                    />
+                                </SpeedDial>
+                                {item.on_sale ? <p className="sale">sale</p> : ''}
+                            </Card>
+                        </Grid>
+                    )
                 }
-                {loading && 
-                    [1,2,3,4,5,6].map(rep=>(
+                {loading &&
+                    [1, 2, 3, 4, 5, 6].map(rep => (
                         <Grid item xs={12} md={4} key={rep}>
                             <Card>
                                 <Skeleton variant="rectangular" height={300} />
-                                <Skeleton sx={{mt: 1, mb:0.3}} variant="rectangular" height={16}/>
-                                <Skeleton variant="text"/>
-                                <Skeleton sx={{mb: 1}} variant="text"/>
+                                <Skeleton sx={{ mt: 1, mb: 0.3 }} variant="rectangular" height={16} />
+                                <Skeleton variant="text" />
+                                <Skeleton sx={{ mb: 1 }} variant="text" />
                             </Card>
                         </Grid>
                     ))
-                    }
-                
+                }
+
             </Grid>
         </Grid>
-     );
+    );
 }
- 
+
 export default Products;
